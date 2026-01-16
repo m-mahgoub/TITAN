fb7664eafdf4edbf8579442aa5e2f5ebbc855b94
Mohamed <mmohamed@c2-gpu-018.cm.cluster>
2026-01-16 12:12:58 -0600

    fix torch version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='',
     license='CC-BY-NC-ND 4.0',
     packages=find_packages(exclude=['__dep__', 'assets']),
-    install_requires=["torch==2.0.1", "torchvision==0.15.2", "timm==1.0.3", "einops==0.6.1", "einops-exts==0.0.4", "tqdm==4.66.6", "h5py==3.8.0", "transformers>=4.46.1", "pandas==2.2.3", "scikit-learn==1.5.2", "numpy==1.26.4"],
+    install_requires=["torch>=2.1.0", "torchvision>=0.16.0", "timm==1.0.3", "einops==0.6.1", "einops-exts==0.0.4", "tqdm==4.66.6", "h5py==3.8.0", "transformers>=4.46.1", "pandas==2.2.3", "scikit-learn==1.5.2", "numpy==1.26.4"],
     classifiers = [
     "Programming Language :: Python :: 3",
     "License :: CC-BY-NC-ND 4.0",
